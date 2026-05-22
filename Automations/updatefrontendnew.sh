39bc04d2d37e38989ec594c733c1133d3939d193
Ubuntu <ubuntu@ip-172-31-44-229.ap-south-1.compute.internal>
2026-05-22 09:50:17 +0000

    Updated EC2 instance ID and frontend URL

--- Automations/updatefrontendnew.sh
+++ Automations/updatefrontendnew.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the Instance ID and path to the .env file
-INSTANCE_ID="i-073ad500c7d3b77d2"
+INSTANCE_ID="i-047ba7043a37d3b72"
 
 # Retrieve the public IP address of the specified EC2 instance
 ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
